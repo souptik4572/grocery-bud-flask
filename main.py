792f0beaf30ba9c5a37a3fa2e89ddf8ed59c22c6
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-07 00:10:00 +0530

    removed unnecessary imports

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 import jwt
-from sqlalchemy.orm import backref
 import bcrypt
 import jwt
 
