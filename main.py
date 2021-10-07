9b33c19c067b8ef13aff254e4d8a22588f32ef13
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-07 10:12:12 +0530

    trying postgres with flask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 import jwt
 import bcrypt
 import jwt
@@ -9,15 +10,15 @@ from decouple import config
 
 SECRET_KEY = config('ACCESS_SECRET_TOKEN')
 BCRYPT_SALT = int(config('BCRYPT_SALT'))
-# DATABASE_URL = config('DATABASE_URL')
+DATABASE_URL = config('DATABASE_URL')
 print(SECRET_KEY)
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
-# if DATABASE_URL:
-#     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 database = SQLAlchemy(app)
+if DATABASE_URL:
+    database = create_engine(DATABASE_URL)
 
 
 class ItemModel(database.Model):
