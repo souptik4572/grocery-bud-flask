7b56ab863e23b577a46a45776b40d8e871936fcf
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-12 09:19:38 +0530

    modified the database schema and integrated postgresql for production

---
+++ base.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from decouple import config
+
+DATABASE_URL = config('DATABASE_URL')
+ENV = config('ENV')
+
+local_database_url = 'sqlite:///items.db'
+if ENV == 'prod':
+    local_database_url = DATABASE_URL
+
+engine = create_engine(local_database_url)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
