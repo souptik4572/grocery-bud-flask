7fe6424b37b5605776b7484d438feb2a800f5fc9
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-12 21:14:12 +0530

    segregated files in separate files

---
+++ models/__init__.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from decouple import config
+
+DATABASE_URL = config('DATABASE_URL')
+ENV = config('ENV')
+
+local_database_url = 'sqlite:///items.db' # for local development
+if ENV == 'prod': # for production environment
+    local_database_url = DATABASE_URL
+
+engine = create_engine(local_database_url)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+Base.metadata.create_all(engine)
+session = Session()
